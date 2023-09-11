fe264b308a9ac4994df88b323b426c65db548996
Suhas NH <92245302+suhas991@users.noreply.github.com>
2023-09-11 22:23:58 +0530

    Time: 3 ms (98.02%), Space: 7.6 MB (6.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        
-        if(s==t){
-            return true;
+        if(s.length()!=t.length()){
+            return false;
         }
-        return false;
+        unordered_map<char,int> mp;
+        
+        for(int i=0;i<s.length();i++){
+           mp[s[i]]++;
+           mp[t[i]]--;
+        }
+        
+        for(int i=0;i<mp.size();i++){
+            if(mp[s[i]]!=0)
+                return false;
+        }
+        return true;
         
     }
 };
