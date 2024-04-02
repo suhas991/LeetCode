70588d7252f5197236b5dd4972087ebd584347c1
Suhas NH <92245302+suhas991@users.noreply.github.com>
2024-04-02 10:28:55 +0530

    Time: 0 ms (100.00%), Space: 11 MB (27.78%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+      
+        int maxi=INT_MIN;
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]>maxi){
+                maxi=candies[i];
+            }
+        }
+        
+        vector<bool> ans;
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]+extraCandies >= maxi){
+                ans.push_back(true);
+            }else{
+                ans.push_back(false);
+            }
+            
+        }
+        return ans;
+    }
+};
