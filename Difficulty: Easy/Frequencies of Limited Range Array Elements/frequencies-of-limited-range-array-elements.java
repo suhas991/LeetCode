5e6190dd6314bc53f2d1b6ed3a183a275c77925a
Suhas N H <92245302+suhas991@users.noreply.github.com>
2024-09-07 06:12:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // testcases
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+
+            // size of array
+            int N = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[N];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // adding elements to the array
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int P = Integer.parseInt(br.readLine().trim());
+            // calling frequncycount() function
+            Solution ob = new Solution();
+            ob.frequencyCount(arr, N, P);
+
+            // printing array elements
+            for (int i = 0; i < N; i++) System.out.print(arr[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to count the frequency of all elements from 1 to N in the array.
+    public static void frequencyCount(int arr[], int N, int P) {
+        // do modify in the given array
+        Map<Integer,Integer> mp = new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);
+            arr[i]=0;
+        }
+        
+        for(int key:mp.keySet()){
+            
+            //System.out.println("Key :"+key+"value"+mp.get(key));
+            if(key <= N)
+            arr[key-1]=mp.get(key);
+        }
+        
+    }
+}
